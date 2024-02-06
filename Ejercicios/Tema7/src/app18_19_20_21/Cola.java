d42390ad69d9b334c20a4cbc82eaf1348938d828
pabrodpen <pabrodpen@gmail.com>
2024-02-06 10:01:47 +0100

    act

--- Ejercicios/Tema7/src/app18_19_20_21/Cola.java
+++ Ejercicios/Tema7/src/app18_19_20_21/Cola.java
@@ -1,5 +1,18 @@
 package app18_19_20_21;
 
+import java.util.Arrays;
+
 public class Cola {
 
+	Integer[]cola=new Integer[0];
+	
+	void encolar(Integer n) {
+		cola=Arrays.copyOf(cola, cola.length+1);
+		cola[cola.length-1]=n;
+	}
+	
+	void desencolar() {
+		System.arraycopy(cola, 1, cola, 0, cola.length-1);
+		cola=Arrays.copyOf(cola, cola.length-1);
+	}
 }
