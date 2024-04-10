532284cbbebb53379e52d58e914cdcf65f056d8c
pabrodpen <pabrodpen@gmail.com>
2024-04-10 11:12:32 +0200

    act

--- Ejercicios/Tema11/src/app24/Main.java
+++ Ejercicios/Tema11/src/app24/Main.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +18,9 @@ public class Main {
 
 		llenarArray("fich1");
 		llenarArray("fich2");
+		
+		add("src/app24/fich1.dat", "src/app24/fich2.dat");
+		
 		
 	}
 	
@@ -34,9 +38,42 @@ public class Main {
 			Integer[]l2=(Integer[]) inNuevo.readObject();
 			
 			inBase.close();
-			inBase.close();
+			inNuevo.close();
 			
+			Arrays.sort(l1);
+			Arrays.sort(l2);
+			
+			boolean repetido=false;
+			for(int i=0;i<l2.length;i++) {
+				for(int j=0;j<l1.length && !repetido;j++) {
+					if(l2[i]==l1[j]) {
+						repetido=true;
+					}
+				}
+				if(repetido) {
+					repetido=false;
+				}else {
+					l1=Arrays.copyOf(l1, l1.length+1);
+					l1[l1.length-1]=l2[i];	
+				}
+					
+			}
+			
+			Arrays.sort(l1);
 			ObjectOutputStream outBase=new ObjectOutputStream(new FileOutputStream(fBase));
+		
+			outBase.writeObject(l1);
+			outBase.close();
+			
+			
+			//comprobamos leyendo de nuevo fich1
+			
+			ObjectInputStream inBase2=new ObjectInputStream(new FileInputStream(fBase));
+			Integer[]res=(Integer[]) inBase2.readObject();
+			
+			inBase2.close();
+			System.out.println("ARRAY AÑADIDO");
+			System.out.println(Arrays.toString(res));
 		} catch (IOException | ClassNotFoundException e) {
 			// TODO: handle exception
 			System.out.println(e.getMessage());
@@ -56,7 +93,7 @@ public class Main {
 	        }
 	        
 	        try {
-	            ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream("src/app23/"+f+".dat"));
+	            ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream("src/app24/"+f+".dat"));
 	            
 	            out.writeObject(lista);
 	            out.close();
---
+++ Ejercicios/Tema11/src/app25/Main.java
@@ -0,0 +1,113 @@
+package app25;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		//introducimos valores en los ficheros
+		
+		Integer[]l1= {2,3,5};
+		Integer[]l2= {23,11,56,0};
+		Integer[]l3= {1,-7,66,-21,7};
+		Integer[]l4= {-5,78};
+		ObjectInputStream in1,in2,in3,in4;
+		ObjectOutputStream out1,out2,out3,out4;
+		try {
+			 out1=new ObjectOutputStream(new FileOutputStream("src/app25/fich1.dat"));
+			 out2=new ObjectOutputStream(new FileOutputStream("src/app25/fich2.dat"));
+			 out3=new ObjectOutputStream(new FileOutputStream("src/app25/fich3.dat"));
+			 out4=new ObjectOutputStream(new FileOutputStream("src/app25/fich4.dat"));
+
+			out1.writeObject(l1);
+			out2.writeObject(l2);
+			out3.writeObject(l3);
+			out4.writeObject(l4);
+			
+			out1.close();
+			out2.close();
+			out3.close();
+			out4.close();
+			
+			Scanner scanner=new Scanner(System.in);
+			
+			
+			
+			System.out.print("Dime un numero:");
+			Integer n=scanner.nextInt();
+			
+			
+			Integer[]listaNum1,listaNum2,listaNum3,listaNum4;
+			
+			in1=new ObjectInputStream(new FileInputStream("src/app25/fich1.dat"));
+			in2=new ObjectInputStream(new FileInputStream("src/app25/fich2.dat"));
+			in3=new ObjectInputStream(new FileInputStream("src/app25/fich3.dat"));
+			in4=new ObjectInputStream(new FileInputStream("src/app25/fich4.dat"));
+
+			
+			
+			listaNum1=(Integer[]) in1.readObject();
+			listaNum2=(Integer[]) in2.readObject();
+			listaNum3=(Integer[]) in3.readObject();
+			listaNum4=(Integer[]) in4.readObject();
+			
+			boolean encontrado=false;
+			int numFich=1,pos=0;
+			
+			for(int i=0;i<listaNum1.length && !encontrado;i++) {
+				if(listaNum1[i]==n) {
+					encontrado=true;
+					pos=i;
+				}
+			}
+			
+			numFich++;
+			for(int i=0;i<listaNum2.length && !encontrado;i++) {
+				if(listaNum2[i]==n) {
+					encontrado=true;
+					pos=i;
+				}
+			}
+			
+			numFich++;
+			for(int i=0;i<listaNum3.length && !encontrado;i++) {
+				if(listaNum3[i]==n) {
+					encontrado=true;
+					pos=i;
+				}
+			}
+			
+			numFich++;
+			for(int i=0;i<listaNum4.length && !encontrado;i++) {
+				if(listaNum4[i]==n) {
+					encontrado=true;
+					pos=i;
+				}
+			}
+			
+			if(encontrado) {
+				System.out.println(n+" encontrado en el fichero fich"+numFich+" en la posicion "+pos);
+			}else {
+				System.out.println(n+" no se encuentra en ninguno de los ficheros");
+			}
+			
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+
+		
+		
+		
+		
+	}
+
+}
