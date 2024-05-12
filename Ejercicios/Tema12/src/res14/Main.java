70956f64fbbcc1b79eb8647f8dce32d2fefedbda
pabrodpen <pabrodpen@gmail.com>
2024-05-13 00:25:51 +0200

    act

---
+++ Ejercicios/Tema12/src/res14/Main.java
@@ -0,0 +1,82 @@
+package res14;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static  void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion=0;
+
+        Set<Registro> cjtoRegistros=new HashSet<>();
+
+        while(opcion!=4) {
+            System.out.println("1.Nuevo registro");
+            System.out.println("2.Listar registro");
+            System.out.println("3.Mostrar estadistica");
+            System.out.println("4.Salir");
+            System.out.print("Dime una opcion:");
+            opcion=sc.nextInt();
+
+            switch(opcion) {
+                case 1:
+                    System.out.print("Temperatura:");
+                    double temperatura=sc.nextDouble();
+
+                    String hora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+                    if(cjtoRegistros.add(new Registro(temperatura))==false){
+                        System.out.println("El registro ya existe");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("REGISTROS");
+                    System.out.println(cjtoRegistros);
+                    break;
+
+                case 3:
+                    double sumaTmp=0;
+                    double maxTmp=Double.MIN_VALUE,minTmp=Double.MAX_VALUE;
+                    for(Registro registro:cjtoRegistros){
+                        if(registro.getTemperatura()>maxTmp){
+                            maxTmp=registro.getTemperatura();
+                        }
+
+                        if(registro.getTemperatura()<minTmp){
+                            minTmp=registro.getTemperatura();
+                        }
+
+                        sumaTmp+=registro.getTemperatura();
+                    }
+
+                    System.out.println("Temperatura maxima:"+maxTmp);
+                    System.out.println("Temperatura minima:"+minTmp);
+                    double media=(double) sumaTmp/cjtoRegistros.size();
+                    System.out.println("Promedio:"+media);
+                    break;
+
+                case 4:
+                    try{
+                        String fecha=DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDateTime.now());
+                        ObjectOutputStream out=new ObjectOutputStream(new FileOutputStream("src/res14/registros"+fecha+".dat"));
+
+                        out.writeObject(cjtoRegistros);
+                        out.close();
+                    }catch (IOException e){
+                        System.out.println(e.getMessage());
+                    }
+            }
+
+        }
+    }
+}
---
+++ Ejercicios/Tema12/src/res14/Registro.java
@@ -0,0 +1,38 @@
+package res14;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Registro implements Serializable {
+
+    LocalDateTime hora;
+    double temperatura;
+
+
+    public Registro(double temperatura) {
+        this.hora = LocalDateTime.now();
+        this.temperatura = temperatura;
+    }
+
+    public LocalDateTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalDateTime hora) {
+        this.hora = hora;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro{hora=" + hora + ", temperatura=" + temperatura +"}\n";
+    }
+}
